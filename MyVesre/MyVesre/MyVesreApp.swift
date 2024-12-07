e488ce38129e1c2ad17dead403dd69cdbdbc38f4
fine <92whdtjs@gmail.com>
2024-12-07 11:17:35 +0900

    Initial Xcode Project Setting

---
+++ MyVesre/MyVesre/MyVesreApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyVesreApp.swift
+//  MyVesre
+//
+//  Created by 이종선 on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MyVesreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
