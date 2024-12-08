516a0f59bef6bed765b16204669441bba874cf3f
fine <92whdtjs@gmail.com>
2024-12-08 22:40:58 +0900

    chore: #1 - configure Firebase with AppDelegate

--- MyVesre/MyVesre/App/Application/AppDelegate.swift
+++ MyVesre/MyVesre/App/Application/AppDelegate.swift
@@ -5,4 +5,14 @@
 //  Created by 이종선 on 12/7/24.
 //
 
-import Foundation
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
